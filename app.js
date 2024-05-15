f5db464622ed3270e51b080ab620d031adf8aa61
Usih Elijah <princeusih@gmail.com>
2024-05-15 22:26:37 +0100

    InitialCommit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send(`You can post to this endpoint...`);
+});
+
+const PORT = 4000;
+// const HOST = `127.0.0.1`;
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`);
+});
