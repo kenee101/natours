aecfb0eb0c93b97871f4f71db03b99268ff22159
Usih Elijah <princeusih@gmail.com>
2024-05-29 18:10:01 +0100

    SecondCommit

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,12 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(/*tourController.checkBody*/ tourController.createTour);
 
 router
   .route('/:id')
--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const { default: mongoose } = require('mongoose');
 const app = require('./app');
 
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
 
 const PORT = process.env.PORT || 4000;
 // const HOST = `127.0.0.1`;
