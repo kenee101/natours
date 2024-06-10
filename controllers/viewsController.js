ce684d5916eb0490ea49b1cb87b15d483b60a413
Usih Elijah <princeusih@gmail.com>
2024-06-10 22:45:03 +0100

    NinthCommit

---
+++ controllers/viewsController.js
@@ -0,0 +1,21 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build the template
+
+  // 3) Render the template using the tour data
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+};
