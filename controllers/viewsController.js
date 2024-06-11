303e409281543a8a641b874687757fe0288eb8b0
kenee101 <elijah.usih@stu.cu.edu.ng>
2024-06-11 17:17:52 +0100

    TenthCommit

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,7 +5,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  // 2) Build the template
+  // 2) Build the template in the pug
 
   // 3) Render the template using the tour data
   res.status(200).render('overview', {
@@ -14,8 +14,19 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }); //.populate({
+  // path: 'reviews',
+  // fields: 'review rating user',
+  // });
+
+  // 2) Build the template in the pug
+
+  // 3) Render the template using the provided data
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
