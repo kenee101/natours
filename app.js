c4a36d9603f193b4417467a66a85c406bb0ff8e9
Usih Elijah <princeusih@gmail.com>
2024-06-17 13:25:43 +0100

    setting-proxies

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const bookingController = require('./controllers/bookingController');
 const app = express();
 
 // app.enable('trust proxy');
-app.set('trust proxy', 1);
-app.get('/ip', (req, res) => res.json({ ip: req.ip }));
+app.set('trust proxy', 3);
+// app.get('/ip', (req, res) => res.json({ ip: req.ip }));
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/src/views'));
